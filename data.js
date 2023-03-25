72f752bee22905f50db53d22f028d428f21f5bdc
hsuntarq <hsuntariq@gmail.com>
2023-03-25 12:59:09 +0500

    data

---
+++ data.js
@@ -0,0 +1,27 @@
+export const data = [{
+        id: 1,
+        name: 'Abstract Image 1',
+        image: 'https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992__340.jpg',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatem, quas ipsum ratione natus dignissimos aliquid sit. Ducimus quibusdam fuga soluta nisi non molestiae nulla, ab recusandae totam, odit consectetur.',
+    },
+    {
+        id: 2,
+        name: 'Abstract Image 2',
+        image: 'https://images.unsplash.com/photo-1590621819878-3fa04495626c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGFydHdvcmt8ZW58MHx8MHx8&w=1000&q=80',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatem, quas ipsum ratione natus dignissimos aliquid sit. Ducimus quibusdam fuga soluta nisi non molestiae nulla, ab recusandae totam, odit consectetur.',
+    },
+    {
+        id: 3,
+        name: 'Abstract Image 3',
+        image: 'https://img.freepik.com/premium-photo/abstract-3d-illustration-colorful-paint-background_562847-565.jpg?w=2000',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatem, quas ipsum ratione natus dignissimos aliquid sit. Ducimus quibusdam fuga soluta nisi non molestiae nulla, ab recusandae totam, odit consectetur.',
+
+    },
+    {
+        id: 4,
+        name: 'Abstract Image 3',
+        image: 'https://img.freepik.com/premium-photo/abstract-3d-illustration-colorful-paint-background_562847-565.jpg?w=2000',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatem, quas ipsum ratione natus dignissimos aliquid sit. Ducimus quibusdam fuga soluta nisi non molestiae nulla, ab recusandae totam, odit consectetur.',
+
+    },
+]
